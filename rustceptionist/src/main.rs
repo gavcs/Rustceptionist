418634ca6a219d180ea9ac343059e8d0790008ee
gavcs <gavinwmcconnell@gmail.com>
2024-08-27 10:38:55 -0400

    trying to figure out how to handle get

--- rustceptionist/src/main.rs
+++ rustceptionist/src/main.rs
@@ -1,32 +1,38 @@
 use std::{
-    fs, io::{prelude::*, BufReader}, net::{TcpListener, TcpStream}, thread
+    fs, io::{prelude::*, BufReader}, net::{TcpListener, TcpStream}, thread,
 };
+
+//fn handle_get()
 
 fn handle(mut stream: TcpStream) {
     let buf = BufReader::new(&mut stream);
     let req = buf.lines().next();
 
-    let req = match req {
+    let request = match req {
         Some(val) => val,
         None => {
             return;
         }
     };
-    let req = match req {
+
+    let finreq = match request {
         Ok(val) => val,
-        Err(e) =>{
+        Err(e) => {
             eprintln!("{e}");
             return;
         }
     };
 
-    let (status, fname) = match &req[..] {
+    let (status, fname) = match &(finreq.as_str())[..] {
         "GET / HTTP/1.1" => ("HTTP/1.1 200 OK", "./src/html/hello.html"),
         "GET /secret HTTP/1.1" => ("HTTP/1.1 200 OK", "./src/html/secret.html"),
-        _ => ("HTTP/1.1 404 NOT FOUND", "./src/html/404.html"),
+        _ => {
+            println!("{finreq}");
+            ("HTTP/1.1 404 NOT FOUND", "./src/html/404.html")
+        }
     };
 
-    let content = fs::read_to_string(fname);
+    let content = fs::read_to_string("./src/html/hello.html");
     let content = match content {
         Ok(val) => val,
         Err(e) => {
@@ -36,7 +42,7 @@ fn handle(mut stream: TcpStream) {
     };
     let len = content.len();
 
-    let response = format!("{status}\r\nContent-Length: {len}\r\n\r\n{content}");
+    let response = format!("HTTP/1.1 200 OK\r\nContent-Length: {len}\r\n\r\n{content}");
 
     stream.write_all(response.as_bytes()).unwrap_or_else(|err| {
         eprintln!("{err}");
@@ -45,6 +51,7 @@ fn handle(mut stream: TcpStream) {
 
 fn main() {
     let listen = TcpListener::bind("127.0.0.1:7878").unwrap();
+    
 
     for stream in listen.incoming() {
         let unwrapped_stream = match stream {
